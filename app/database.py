47f4136234eb565168897b6cffd733f7f14601b4
meshrr <volframhome@gmail.com>
2025-06-13 16:46:38 +0500

    db fix

--- app/database.py
+++ app/database.py
@@ -17,7 +17,7 @@ from app.models import (
     Level, L2OrderBook, Transaction as PydanticTransaction
 )
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@db:5432/stock_exchange")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:kirill18032005@db:5432/stock_exchange")
 
 engine = create_engine(DATABASE_URL)
 
